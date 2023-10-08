5b2409d3313b77f3688d6fdad9a3b295607575d3
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-08 12:04:51 +0900

    ベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # ベーシック認証の環境変数を読み込む記述
+    end
+  end
 end
