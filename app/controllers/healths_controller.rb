18a6dea35808ea08e54c8b28ad2ecbd3d4c81f47
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-12 20:37:00 +0900

    回答コントローラーでの一覧・生成・保存

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -1,5 +1,6 @@
 class HealthsController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @healths = Health.all
   end
@@ -9,6 +10,19 @@ class HealthsController < ApplicationController
   end
 
   def create
+    @health = Health.new(health_params)
+    if @health.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+
+  private
+
+  def health_params
+    params.require(:health).permit(:stress , :stress_score , :refresh , :refresh_score ,:assess_id , :weight_id , :image).merge(user_id: current_user.id)
+  end
+
 end
