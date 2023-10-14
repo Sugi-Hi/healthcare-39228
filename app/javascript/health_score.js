ab41ef063ee19d19a9e31d0101a4f6446f7d979b
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-14 23:10:53 +0900

    点数表示の為のJavaScript活用

---
+++ app/javascript/health_score.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+
+  // ストレス度合・優先順位を選択した点数をstressInputという変数へ格納
+  const stressInput = document.getElementById("stress-score");
+  const refreshInput = document.getElementById("refresh-score");
+  // コンソール起動でログでの表示確認
+  console.log(stressInput)
+  console.log(refreshInput)
+
+  stressInput.addEventListener("input", () => {
+    const inputStressScore = stressInput.value;
+
+    const addStressScore = document.getElementById("add-stress-score");
+    addStressScore.innerHTML = inputStressScore
+  })
+
+  refreshInput.addEventListener("input", () => {
+    const inputRefreshScore = refreshInput.value;
+
+    const addRefreshScore = document.getElementById("add-refresh-score");
+    addRefreshScore.innerHTML = inputRefreshScore
+  })
+
+
+
+});
