2b07db0d8e9c4ff8dbdd8cdb714ecf317b5fc52e
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-17 16:57:37 +0900

    JSファイル保留

--- app/javascript/health_score.js
+++ app/javascript/health_score.js
@@ -1,20 +1,20 @@
 window.addEventListener('load', () => {
 
   // ストレス度合・優先順位を選択した点数をstressInputという変数へ格納
-  // const stressInput = document.getElementById("stress-score");
-  // const refreshInput = document.getElementById("refresh-score");
+  const stressInput = document.getElementById("stress-score");
+  const refreshInput = document.getElementById("refresh-score");
   // コンソール起動でログでの表示確認
   console.log("ストレス")
   console.log("リフレッシュ")
 
-  // stressInput.addEventListener("input", () => {
+  // stressInput.addEventListener("change", () => {
   //   const inputStressScore = stressInput.value;
 
   //   const addStressScore = document.getElementById("add-stress-score");
   //   addStressScore.innerHTML = inputStressScore
   // })
 
-  // refreshInput.addEventListener("input", () => {
+  // refreshInput.addEventListener("change", () => {
   //   const inputRefreshScore = refreshInput.value;
 
   //   const addRefreshScore = document.getElementById("add-refresh-score");
