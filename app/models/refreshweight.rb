e799908e8c541e2846498e6a5775bbf51d5c0da9
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-17 21:10:44 +0900

    編集ページの作成

--- app/models/refreshweight.rb
+++ app/models/refreshweight.rb
@@ -1,9 +1,9 @@
 class Refreshweight < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '1.通常,,' },
-    { id: 2, name: '2.関心→' },
-    { id: 3, name: '3.大切!!' },
+    { id: 1, name: '1.通常,特に考えず何となくする。' },
+    { id: 2, name: '2.関心→日頃から好きでやってる！' },
+    { id: 3, name: '3.大切!常に生きがいになってる！' },
  
 
   ]
--- app/models/stressweight.rb
+++ app/models/stressweight.rb
@@ -1,9 +1,9 @@
 class Stressweight < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '1.不問..' },
-    { id: 2, name: '2.注目.!' },
-    { id: 3, name: '3.重要!!' },
+    { id: 1, name: '1.不問.別に気にしなくても良い。' },
+    { id: 2, name: '2.注意!気になるが何とかなる状況。' },
+    { id: 3, name: '3.重大!気を付けねばならぬ事情！'},
  
 
   ]
