1b46a337e89803e06c099b4f462eb97562bad67c
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-18 18:46:12 +0900

    ケア対策・選定ページでのマイグレーション

--- db/migrate/20231018014245_create_cares.rb
+++ db/migrate/20231018014245_create_cares.rb
@@ -1,6 +1,13 @@
 class CreateCares < ActiveRecord::Migration[6.0]
   def change
     create_table :cares do |t|
+      t.date          :care_date,     null: false
+      t.datetime      :cate_time
+      t.text          :care_place
+      t.string        :go_by
+      t.integer       :caremethod_id, null: false
+      t.references    :user         , null: false, foreign_key: true
+      t.references    :health       , null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_12_063158) do
+ActiveRecord::Schema.define(version: 2023_10_18_014245) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 2023_10_12_063158) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cares", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "care_date", null: false
+    t.datetime "cate_time"
+    t.text "care_place"
+    t.string "go_by"
+    t.integer "caremethod_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "health_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["health_id"], name: "index_cares_on_health_id"
+    t.index ["user_id"], name: "index_cares_on_user_id"
   end
 
   create_table "healths", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -74,5 +88,7 @@ ActiveRecord::Schema.define(version: 2023_10_12_063158) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cares", "healths"
+  add_foreign_key "cares", "users"
   add_foreign_key "healths", "users"
 end
