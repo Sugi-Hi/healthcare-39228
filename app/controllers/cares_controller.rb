758c3ffcf738f7b0897945859b7c126e76ce6e13
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-18 18:47:14 +0900

    ケアへの対策選定に必要な選択肢の設定

--- app/controllers/cares_controller.rb
+++ app/controllers/cares_controller.rb
@@ -1,2 +1,16 @@
 class CaresController < ApplicationController
+
+  def index
+    
+  end
+   
+  def create
+
+  end
+
+  private
+
+  def care_params
+  end
+
 end
--- app/models/care.rb
+++ app/models/care.rb
@@ -1,2 +1,12 @@
 class Care < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :caremethod
+
+
+  validates :care_date    , presence: true
+  validates :caremethod_id, presence: true
+
+  belongs_to :user
+  belongs_to :health
 end
