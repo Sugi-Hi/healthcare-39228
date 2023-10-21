412ecc787759dcdb096f99363eab022683ea6b53
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-21 22:59:01 +0900

    優先順位わかりやすさへの考案

--- app/models/refreshweight.rb
+++ app/models/refreshweight.rb
@@ -2,7 +2,7 @@ class Refreshweight < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
     { id: 1, name: '1.通常,特に考えず何となくする。' },
-    { id: 2, name: '2.関心→日頃から好きでやってる！' },
+    { id: 2, name: '2.興味→日頃から好きでやってる！' },
     { id: 3, name: '3.大切!常に生きがいになってる！' },
  
 
--- app/models/stressweight.rb
+++ app/models/stressweight.rb
@@ -2,8 +2,8 @@ class Stressweight < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
     { id: 1, name: '1.不問.別に気にしなくても良い。' },
-    { id: 2, name: '2.注意!気になるが何とかなる状況。' },
-    { id: 3, name: '3.重大!気を付けねばならぬ事情！'},
+    { id: 2, name: '2.注意!気にはなるが何とかなる状況。' },
+    { id: 3, name: '3.重大!気を付けねばならぬ事情・立場！'},
  
 
   ]
