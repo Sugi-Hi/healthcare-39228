af3ba9f1f93b790562075207f29ccb60413f6b35
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-11-22 16:51:45 +0900

    careデータベースのわかりやすい目印言葉

--- db/migrate/20231018014245_create_cares.rb
+++ db/migrate/20231018014245_create_cares.rb
@@ -1,11 +1,16 @@
 class CreateCares < ActiveRecord::Migration[6.0]
   def change
     create_table :cares do |t|
+      # 対策の日程(必須)・時間帯(任意)
       t.date          :care_date,     null: false
       t.time          :care_time
+      # 対策の場所(任意)
       t.text          :care_place
+      # 対策の為の交通手段(任意)
       t.string        :go_by
+      # 対策手段の選択(必須)
       t.integer       :caremethod_id, null: false
+      
       t.references    :user         , null: false, foreign_key: true
       t.references    :health       , null: false, foreign_key: true
 
