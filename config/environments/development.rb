a16b21171584f41387194129db4d15ffe6e92827
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-04-05 10:18:15 +0900

    画像データベース設定のamazon変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon  # ←:local
+  config.active_storage.service = :amazon  # ←:local(変更)
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
