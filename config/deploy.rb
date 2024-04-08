60f34713b73552dfe6bf2f93dedc7dc362e700ea
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-04-08 11:36:14 +0900

    deployファイル修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock 'capistrano (3.18.1)'
 set :application, 'healthcare-39228'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/Sugi-Hi/healthcare-39228.git'
+set :repo_url,  'git@github.com:Sugi-Hi/healthcare-39228.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
