e924a68800c5f77297ea7ae7f19673cb84eac967
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-04-17 18:19:17 +0900

    JSファイルのコンソール表示変更

--- app/javascript/health_score.js
+++ app/javascript/health_score.js
@@ -4,11 +4,12 @@ window.addEventListener('load', () => {
   const stressInput = document.getElementById("stress-score");
   const refreshInput = document.getElementById("refresh-score");
   // コンソール起動でログでの表示確認
-  console.log("ストレス")
-  console.log("リフレッシュ")
+  console.log("ストレスレベル")
+  console.log("リフレッシュランク")
 
   stressInput.addEventListener("input", () => {
     const inputStressScore = stressInput.value;
+    console.log("ストレス値")
 
     const addStressScore = document.getElementById("add-stress-score");
     addStressScore.innerHTML = inputStressScore
@@ -16,6 +17,7 @@ window.addEventListener('load', () => {
 
   refreshInput.addEventListener("input", () => {
     const inputRefreshScore = refreshInput.value;
+    console.log("リフレッシュ値")
 
     const addRefreshScore = document.getElementById("add-refresh-score");
     addRefreshScore.innerHTML = inputRefreshScore
